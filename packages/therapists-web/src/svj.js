099b9d1b0f47c134c2c0570ab172cb6e8f40c8aa
Rohitash Goyal <32011743+Rohitash28@users.noreply.github.com>
2021-01-29 01:22:26 +0530

    svg

---
+++ packages/therapists-web/src/svj.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: '',// React Module location
+  module: {
+    rules: [
+      //...
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+
+};
