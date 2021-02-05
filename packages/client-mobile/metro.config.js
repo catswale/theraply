fafb8fa9d1c86e0b216b0a78280c0a726916192a
catswale <catswale@gmail.com>
2021-02-05 15:53:30 +0800

    Add react native svg

--- packages/client-mobile/metro.config.js
+++ packages/client-mobile/metro.config.js
@@ -1,9 +1,20 @@
 const { createMetroConfiguration } = require('expo-yarn-workspaces');
+const { getDefaultConfig } = require('metro-config');
 const blacklist = require('metro-config/src/defaults/blacklist');
 
-module.exports = {
-  ...createMetroConfiguration(__dirname),
-  resolver: {
-    blacklistRE: blacklist([/lib\/amplify\/.*/])
-  }
-}
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    ...createMetroConfiguration(__dirname),
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      blacklistRE: blacklist([/lib\/amplify\/.*/]),
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg']
+    },
+  };
+})();
