4eee992a030d9868ae4f779e100e4868b5c9800d
Harrison Favour <fayvorharrison101@gmail.com>
2021-02-10 22:46:37 +0100

    make width of svg dynamic based on total steps

--- packages/client-mobile/src/assets/WizardStep.tsx
+++ packages/client-mobile/src/assets/WizardStep.tsx
@@ -10,11 +10,13 @@ interface WizardStepProps {
 const WizardStep = ({ stepNumber, totalSteps = 3 }: WizardStepProps) => {
   const getActiveColor = (index: number) => index === stepNumber ? palette.primary.main : palette.lineColor;
 
+  const rectWidth = 15;
+  const width = (totalSteps * rectWidth) + ((totalSteps - 1) * 5);
   return (
-    <Svg width="75" height="5" viewBox="0 0 75 5" fill="none">
+    <Svg width={width} height="5" viewBox={`0 0 ${width} 5`} fill="none">
       {
         Array(totalSteps).fill(1).map((_, i) => (
-          <Rect key={i} x={i * 20} width="15" height="5" rx="2.5" fill={getActiveColor(i + 1)} />
+          <Rect key={i} x={i * 20} width={rectWidth} height="5" rx="2.5" fill={getActiveColor(i + 1)} />
         ))
       }
     </Svg>
