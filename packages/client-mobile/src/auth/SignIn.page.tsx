da8cd2e4a77b880e9f02f7531e86eabd705e3749
catswale <catswale@gmail.com>
2021-02-12 16:57:56 +0800

    update sign in. fix keyboard moving and bunching higher elements

--- packages/client-mobile/src/auth/SignIn.page.tsx
+++ packages/client-mobile/src/auth/SignIn.page.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import {
   View, Text, StyleSheet, TextInput, KeyboardAvoidingView,
-  ViewStyle, TouchableOpacity, TextStyle, Dimensions,
+  ViewStyle, TouchableOpacity, TextStyle, Dimensions, Platform,
+  TouchableWithoutFeedback, Button, Keyboard
 } from 'react-native'
 import {palette} from '@theraply/lib'
 import {theme} from '../theme'
@@ -44,80 +45,82 @@ export const SignIn = ({route, navigation}) => {
   const buttonStyle = disabled ? theme.primaryButtonDisabled : theme.primaryButton
   const graphicWidth = width * 0.5
   return (
-    <View style={styles.container}>
-      <View style={styles.headerTextContainer}>
-        <Text style={theme.subTitle}>Welcome back!</Text>
-        <Text style={theme.title}>Please log in</Text>
-      </View>
-      {
-        loading && <Loading/>
-      }
-      {
-        !loading && 
-        <KeyboardAvoidingView style={styles.bodyContainer} behavior="padding">
+    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          
+          <View style={styles.headerTextContainer}>
+            <Text style={theme.subTitle}>Welcome back!</Text>
+            <Text style={theme.title}>Please log in</Text>
+          </View>
+          <View style={styles.bodyContainer}>
           <Corner style={{position: 'absolute', bottom: 0}} width={118} height={121}/>
-          <View style={styles.graphicView}>
-            <EnterTextGraphic />
-          </View>
-          <View>
-            <Text style={{color: 'red', marginBottom: 10}}>{error}</Text>
-            <Text style={styles.h4}>Email Address</Text>
-            <TextInput
-              placeholder='example@gmail.com'
-              autoCorrect={false}
-              onSubmitEditing={() => { secondInput?.focus() }}
-              autoCapitalize='none'
-              autoCompleteType='email'
-              keyboardType='email-address'
-              textContentType='emailAddress'
-              returnKeyType={'next'}
-              blurOnSubmit={ false }
-              style={{...theme.inputText, marginBottom: 40}}
-              onChangeText={text => {
-                updateButtonState(text, password)
-                setEmail(text)
-              }}
-              value={email}
-            />
-            <Text style={styles.h4}>Password</Text>
-            <TextInput
-              ref={(input) => { onChangeSecondInput(input) }}
-              autoCorrect={false}
-              autoCapitalize='none'
-              autoCompleteType='password'
-              textContentType='password'
-              secureTextEntry={true}
-              style={theme.inputText}
-              onChangeText={text => {
-                updateButtonState(email, text)
-                setPassword(text)
-              }}
-              value={password}
-            />
-          </View>
+            <Text style={{color: 'red'}}>{error}</Text>
+            <View style={styles.graphicView}>
+                <EnterTextGraphic width={width * 0.6}/>
+              </View>
+            <View>
+              <View>
+                <Text style={styles.h4}>Email Address</Text>
+                <TextInput
+                  placeholder='example@gmail.com'
+                  autoCorrect={false}
+                  onSubmitEditing={() => { secondInput?.focus() }}
+                  autoCapitalize='none'
+                  autoCompleteType='email'
+                  keyboardType='email-address'
+                  textContentType='emailAddress'
+                  returnKeyType={'next'}
+                  blurOnSubmit={ false }
+                  style={{...theme.inputText, marginBottom: 40}}
+                  onChangeText={text => {
+                    updateButtonState(text, password)
+                    setEmail(text)
+                  }}
+                  value={email}
+                />  
+              </View>
+              <View>
+              <Text style={styles.h4}>Password</Text>
+              <TextInput
+                ref={(input) => { onChangeSecondInput(input) }}
+                autoCorrect={false}
+                autoCapitalize='none'
+                autoCompleteType='password'
+                textContentType='password'
+                secureTextEntry={true}
+                style={theme.inputText}
+                onChangeText={text => {
+                  updateButtonState(email, text)
+                  setPassword(text)
+                }}
+                value={password}
+              /> 
+              <TouchableOpacity
+                disabled={disabled}
+                style={{...buttonStyle, marginTop: 24}}
+                onPress={signIn}
+              >
+                <Text style={theme.primaryButtonText}>Done</Text>
+              </TouchableOpacity>
 
-          <TouchableOpacity
-            disabled={disabled}
-            style={{...buttonStyle, marginTop: 24}}
-            onPress={signIn}
-          >
-            <Text style={theme.primaryButtonText}>Done</Text>
-          </TouchableOpacity>
-        </KeyboardAvoidingView>
-      }
-      
-    </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   )
 }
 
 interface Style {
   container: ViewStyle,
+  inner: ViewStyle,
   headerTextContainer: ViewStyle,
   bodyContainer: ViewStyle,
   graphicView: ViewStyle,
   lowerBodyContainer: ViewStyle,
   middleBodyContainer: ViewStyle,
-  tickComplete: ViewStyle,
   welcomeText: TextStyle,
   title: TextStyle,
   h4: TextStyle,
@@ -125,8 +128,14 @@ interface Style {
 
 const styles = StyleSheet.create<Style>({
   container: {
-    height: '100%',
+    // height: '100%',
     backgroundColor: palette.secondary.main,
+    flex: 1
+  },
+  inner: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    width: '100%'
   },
   headerTextContainer: {
     justifyContent: 'center',
@@ -135,11 +144,11 @@ const styles = StyleSheet.create<Style>({
     paddingTop: 30,
   },
   bodyContainer: {
-    display: 'flex',
+    flex: 1,
     flexDirection: 'column',
     height: '85%',
     width: '100%',
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     justifyContent: 'space-evenly',
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
@@ -155,9 +164,6 @@ const styles = StyleSheet.create<Style>({
   },
   middleBodyContainer: {
     alignItems: 'center'
-  },
-  tickComplete: {
-    marginBottom: 60,
   },
   welcomeText: {
     color: palette.text.grey2,
