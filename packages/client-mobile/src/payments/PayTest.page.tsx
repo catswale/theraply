33406f1fa483f03d59be9f98f20b64f4d4a036a5
catswale <catswale@gmail.com>
2021-02-18 13:25:43 +0800

    Add expo stripe

--- packages/client-mobile/src/payments/PayTest.page.tsx
+++ packages/client-mobile/src/payments/PayTest.page.tsx
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from 'react';
 import {
   View, Text, StyleSheet, ViewStyle, Button, FlatList,
 } from 'react-native';
-// import { PaymentsStripe } from 'expo-payments-stripe';
-// import {AppleToken, AndroidToken} from 'expo-payments-stripe/src/utils/types'
+import { PaymentsStripe } from 'expo-payments-stripe';
+import { AppleToken, AndroidToken } from 'expo-payments-stripe/src/utils/types';
 import { API, Auth } from 'aws-amplify';
 import { useClient } from '../client/client.hooks';
 
 export const Pay = () => {
-  // const [card, setCard] = useState({} as AppleToken | AndroidToken)
+  const [card, setCard] = useState({} as AppleToken | AndroidToken);
   const [clientSecret, setClientSecret] = useState('pi_1I7CoKLY5UjkiodXddLc3OU4_secret_005NKboRCFXKx1wy1qDf0CUPN');
   const { client } = useClient();
 
   useEffect(() => {
-    if (!client.stripeCustomerID) {
-      // register()
-    }
+    // if (!client.stripeCustomerID) {
+    //   register();
+    // }
   }, []);
 
   async function getCardDetails() {
@@ -34,9 +34,9 @@ export const Pay = () => {
       },
     };
 
-    // const card = await PaymentsStripe.paymentRequestWithCardFormAsync(options);
-    // setCard(card)
-    // console.log(card)
+    const card = await PaymentsStripe.paymentRequestWithCardFormAsync(options);
+    setCard(card);
+    console.log(card);
   }
 
   async function register() {
@@ -72,10 +72,10 @@ export const Pay = () => {
   return (
     <View>
       <Button title='SUBMIT CARD DETAILS' onPress={() => getCardDetails()}/>
-      <Button title='CREATE PAYMENT INTENT' onPress={() => register()}/>
+      {/* <Button title='CREATE PAYMENT INTENT' onPress={() => register()}/>
       <Button title='MAKE PAYMENT' onPress={() => {
 
-      }}/>
+      }}/> */}
     </View>
   );
 };
