21bc711e5d61fa078af0cae06da5fd1420e100a3
catswale <catswale@gmail.com>
2021-03-03 18:53:54 +0800

    cleanup

--- packages/lib/src/graphql/customQueries.ts
+++ packages/lib/src/graphql/customQueries.ts
@@ -1,2 +1,24 @@
-
+export const getTherapist = /* GraphQL */ `
+  query GetTherapist($id: ID!) {
+    getTherapist(id: $id) {
+      id
+      firstName
+      lastName
+      email
+      phoneNumber
+      clients {
+        items {
+          client {
+            id
+            firstName
+            lastName
+          }
+          id
+        }      
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
