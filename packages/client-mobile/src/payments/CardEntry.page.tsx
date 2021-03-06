58d65379e8514fbc0b16f6051fd7b86be5c54f99
catswale <catswale@gmail.com>
2021-03-06 20:27:19 +0800

    Add different keyboard aware view in cardentry

--- packages/client-mobile/src/payments/CardEntry.page.tsx
+++ packages/client-mobile/src/payments/CardEntry.page.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import {
   View, Text, StyleSheet, TextInput, TouchableWithoutFeedback,
-  ViewStyle, TouchableOpacity, TextStyle, KeyboardAvoidingView,
-  Platform, Keyboard,
+  ViewStyle, TouchableOpacity, ScrollView, KeyboardAvoidingView,
+  Platform, Keyboard, Dimensions,
 } from 'react-native';
 import { PaymentsStripe as Stripe } from 'expo-payments-stripe';
 import { palette } from '@theraply/lib';
@@ -11,6 +11,9 @@ import { RouteProp } from '@react-navigation/native';
 import { theme, Background } from '../theme';
 import { RootStackParamList } from '../App';
 import { Loading } from '../components/Loading.page';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+const {height} = Dimensions.get('window');
 
 type ScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -68,54 +71,52 @@ export const CardEntry = ({ route, navigation }: Props) => {
           <Text style={theme.primaryButtonText}>Done</Text>
         </TouchableOpacity>
       }>
-      <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View style={{ height: '100%' }}>
-            <View style={{ flex: 1 }}>
-              <Text style={{ ...theme.boldText, textAlign: 'center' }}>Fill this form</Text>
+      <KeyboardAwareScrollView style={styles.container}>
+        <View style={{ height: height * 0.6 }}>
+          <View style={{ flex: 1 }}>
+            <Text style={{ ...theme.boldText, textAlign: 'center' }}>Fill this form</Text>
+          </View>
+          <View style={{ flex: 2 }}>
+            <Text style={{ color: palette.error.main }}>{error}</Text>
+            <Text style={theme.h4}>Card Number</Text>
+            <TextInput
+              autoCompleteType='cc-number'
+              keyboardType='number-pad'
+              style={{ ...theme.inputText, marginBottom: 40 }}
+              onChangeText={(text) => {
+                setCardNo(text);
+              }}
+              value={cardNo}
+            />
+            <View style={styles.expiryContainer}>
+              <View style={{ flex: 1, paddingRight: 40 }}>
+                <Text style={theme.h4}>Expiry Month</Text>
+                <TextInput
+                  autoCompleteType='cc-exp-month'
+                  keyboardType='number-pad'
+                  style={{ ...theme.inputText }}
+                  onChangeText={(text) => {
+                    setCardMonthExpiry(text);
+                  }}
+                  value={cardMonthExpiry}
+              />
             </View>
-            <View style={{ flex: 2 }}>
-              <Text style={{ color: palette.error.main }}>{error}</Text>
-              <Text style={theme.h4}>Card Number</Text>
+            <View style={{ flex: 1 }}>
+              <Text style={theme.h4}>Expiry Year</Text>
               <TextInput
-                autoCompleteType='cc-number'
+                autoCompleteType='cc-exp-year'
                 keyboardType='number-pad'
                 style={{ ...theme.inputText, marginBottom: 40 }}
                 onChangeText={(text) => {
-                  setCardNo(text);
+                  setCardYearExpiry(text);
                 }}
-                value={cardNo}
+                value={cardYearExpiry}
               />
-              <View style={styles.expiryContainer}>
-                <View style={{ flex: 1, paddingRight: 40 }}>
-                  <Text style={theme.h4}>Expiry Month</Text>
-                  <TextInput
-                    autoCompleteType='cc-exp-month'
-                    keyboardType='number-pad'
-                    style={{ ...theme.inputText }}
-                    onChangeText={(text) => {
-                      setCardMonthExpiry(text);
-                    }}
-                    value={cardMonthExpiry}
-                />
-              </View>
-              <View style={{ flex: 1 }}>
-                <Text style={theme.h4}>Expiry Year</Text>
-                <TextInput
-                  autoCompleteType='cc-exp-year'
-                  keyboardType='number-pad'
-                  style={{ ...theme.inputText, marginBottom: 40 }}
-                  onChangeText={(text) => {
-                    setCardYearExpiry(text);
-                  }}
-                  value={cardYearExpiry}
-                />
-                </View>
               </View>
             </View>
           </View>
-        </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
+        </View>
+      </KeyboardAwareScrollView>
     </Background>
   );
 };
